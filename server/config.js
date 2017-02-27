66bb9f1e523742eba0f2c43d9379e7ac6682649c
Reed Lauber <reedlauber@gmail.com>
2017-02-26 21:15:59 -0800

    Fix config agency slug

--- server/config.js
+++ server/config.js
@@ -20,6 +20,7 @@ try {
   config.agency_settings = require('../config/' + config.agency + '.json');
 } catch(e) {
   config.agency_settings = {};
+  config.agency_settings.slug = config.agency;
   config.agency_settings.app_title = process.env.APP_TITLE || 'NEXT|Transit';
   config.agency_settings.search_text = process.env.SEARCH_TEXT || 'Search for a route ...';
   config.agency_settings.twitter_acct = process.env.TWITTER_ACCT || '';
