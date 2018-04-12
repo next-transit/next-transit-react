1432d905ff06a504f9999d78e9eded9f87c57c73
Reed Lauber <reedlauber@gmail.com>
2018-04-12 15:50:48 -0700

    Use S3 for app JS file.

--- server/config.js
+++ server/config.js
@@ -1,10 +1,15 @@
-var config = {},
-  fs = require('fs');
-  local = {};
+const fs = require('fs');
+const pkg = require('../package.json');
+const config = {};
+
+let local = {};
 
 if(fs.existsSync(__dirname + '/../config/local.json')) {
   local = require('../config/local.json');
 }
+
+const version = pkg.version;
+const versionSlug = version.replace(/\./g, '-');
 
 config.debug_assets = process.env.DEBUG_ASSETS || local.debug_assets;
 config.verbose = process.env.VERBOSE || local.verbose;
@@ -14,6 +19,8 @@ config.mail_password = process.env.MAIL_PASSWORD || local.mail_password;
 config.data_url = process.env.DATA_URL || local.data_url;
 config.data_api_key = process.env.DATA_API_KEY || local.data_api_key;
 config.google_ua_code = process.env.GOOGLE_UA_CODE || local.google_ua_code || '';
+
+config.app_js_path = local.app_js_path || `https://s3.amazonaws.com/next-transit/app-${versionSlug}.js`;
 
 config.agency = process.env.AGENCY || local.agency;
 
