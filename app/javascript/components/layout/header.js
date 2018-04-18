b8ab46b45e229b117b86eaf99f2f5aea859d09c5
Reed Lauber <reedlauber@gmail.com>
2018-04-18 11:45:48 -0700

    Use History for back clicks

--- app/javascript/components/layout/header.js
+++ app/javascript/components/layout/header.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import classnames from 'classnames';
-import { Link } from 'react-router';
+import { browserHistory, Link } from 'react-router';
 
 export default class LayoutHeader extends Component {
   static propTypes = {
@@ -12,6 +12,24 @@ export default class LayoutHeader extends Component {
     title: 'NEXT-Transit',
     back_path: null
   };
+
+  constructor(...args) {
+    super(...args);
+
+    this.onBackClick = this.onBackClick.bind(this);
+  }
+
+  onBackClick(event) {
+    event.preventDefault();
+
+    if (window.history.length) {
+      return browserHistory.goBack();
+    }
+
+    if (this.props.back_path) {
+      browserHistory.push(this.props.back_path);
+    }
+  }
 
   render() {
     let back_classes = classnames('app-header-btn app-header-back-btn', {
@@ -28,6 +46,7 @@ export default class LayoutHeader extends Component {
             to={this.props.back_path}
             className={back_classes} 
             title="Go back"
+            onClick={(event) => { this.onBackClick(event); }}
           >
             <i className="icon-chevron-sign-left"></i>
           </Link>
